f8eb48c5d36d0951978092778c183609b4b9c377
NiekMSoftware <niek+github@meletstudios.com>
2025-08-22 17:09:30 +0200

    base code has been set up

--- src/drawing_a_triangle/main.cpp
+++ src/drawing_a_triangle/main.cpp
@@ -1,29 +1,50 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
 	void run() {
+		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanup();
 	}
 
 private:
+	void initWindow() {
+		glfwInit();
+
+		// Tell GLFW to not create an OpenGL context
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		pWindow = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
 	void initVulkan() {
 		
 	}
 
 	void mainLoop() {
-		
+		while (!glfwWindowShouldClose(pWindow)) {
+			glfwPollEvents();
+		}
 	}
 
 	void cleanup() {
-		
+		glfwDestroyWindow(pWindow);
+
+		glfwTerminate();
 	}
+
+	GLFWwindow* pWindow = nullptr;
 };
 
 int main() {
